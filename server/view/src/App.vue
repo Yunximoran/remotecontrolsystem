6536c7a2fdd0e80471c0531774b35a540f9ee867
yumo <2970368069@qq.com>
2024-10-30 01:01:38 +0800

    update 2024-10-30 end

--- server/view/src/App.vue
+++ server/view/src/App.vue
@@ -1,101 +1,18 @@
 
 <template>
    <!-- 需要输入框，输入shell后回车添加进 -->
-    <dev class="main">
-        <dev class="send select">
-            <dev class="shellbox">
-                <form class="shell_form" @keyup.enter.prevent="addshell(shell)">
-                    <input class="shell_input" type="text" v-model="shell.name">
-                    <br>
-                    <input class="shell_input" type="text" v-model="shell.shell">
-                    <P>{{ shells }}</P>
-                </form>
-                <button @click.prevent="clear(shells)">clear</button>
-                <button @click.prevent="sendshell">send shell</button>
-            </dev>
-
-            <dev class="softwarebox">
-                <form class="softwareform" @submit.prevent="addsoftware(software)">
-                    <input class="softwareinput" type="text" v-model="software.name">
-                    <p>{{ softwares }}</p>
-                </form>
-                <button @click="clear(softwares)">clear</button>
-                <button @click.prevent="sendsoftware">send software</button>
-            </dev>
-        </dev>
-
-        <dev class="output">
-            <span>
-                {{ shells }}
-            </span>
-            <span>
-                {{ softwares }}
-            </span>
-        </dev>
-    </dev>
+    <Sendmodel/>
 
 
 
 
 
 </template>
-
-<script>
-import axios from 'axios';
-export default{
-    data(){
-        return {
-            shells: [],
-            shell: {
-                name: null,
-                shell: null,
-            },
-
-            softwares: [],
-            software: {
-                name: null
-            }
-    }
-  },
-
-    methods: { 
-
-        addshell(shell){
-            console.log(this.shells)
-            if(shell.name && shell.shell){
-                this.shells.push({...shell})
-                shell.name = null
-                shell.shell = null
-            }
-        },
-
-        addsoftware(software){
-            if (software.name){
-                this.softwares.push({...software})
-                software.name = null
-            }
-            
-        },
-
-        clear(obj){
-            obj.length = 0
-        },
-
-        sendshell(){
-            axios.put("/servers/send_control_shell", this.shells).then((res) => {
-                    this.shells = []
-                })
-        },
-
-        sendsoftware(){
-            axios.put("/servers/send_software_checklist/", this.softwares).then((res)=>{
-                this.softwares = []
-            })
-        },
-  }
-}
-
+<script setup>
+import Sendmodel from "./components/sendmodel.vue";
 </script>
+
+
 
 <style>
 #app {
@@ -113,90 +30,10 @@ body {
     /* position: relative; */
 }
 
-.main {
-    display: flex;
-    flex-direction: column;
-    gap:10px;
-}
-
 button {
   background-color: rgb(175, 220, 171);
   color:rgb(21, 73, 55);
   border-radius: 3px;
 }
 
-
-/* set shell form */
-
-.shellbox {
-    float:left;
-    margin: top 5px;
-}
-.shellbox button {
-    float: left;
-    width: 50%;
-    /* margin-left:30px; */
-}
-.shell_form {
-    width: 200px;
-    height: 100px;
-    padding-top: 16%;
-    background-color: #5f8ab5;
-    border: #2c3e50 solid 3px;
-    border-radius: 10px;
-    float: top;
-}
-.shell_input {
-    border-radius: 5px;
-}
-
-
-/* send software */
-.softwarebox {
-    float:left;
-    margin: top 5px;
-}
-.softwarebox button {
-    float: left;
-    width: 50%;
-}
-.softwareform {
-    width: 200px;
-    height: 100px;
-    padding-top: 16%;
-    background-color: #5f8ab5;
-    border: #2c3e50 solid 3px;
-    border-radius: 10px;
-    float: top;
-}
-.softwareinput {
-    border-radius: 5px;
-}
-
-
-/* set output */
-.output {
-    display: flex;
-    flex-direction: row;
-    
-    width: 100%;
-    height: 100%;
-    background-color: green;
-    position: relative;
-    margin-right: 30px;
-}
-
-.output span{
-    width: 100px;
-    height: 100px;
-    background-color: yellow;
-    margin: 10px;
-    /* float: left; */
-    width: 40px;
-    height: 80px;
-}
-
-.output p{
-    color: red;
-}
 </style>
---
+++ server/view/src/components/sendmodel.vue
@@ -0,0 +1,176 @@
+<template>
+    <div class="sendbox">
+        <div class="sends">
+            <div class="send shell">
+                <div class="inputshell">
+                    <input type="text" placeholder="name"
+                        v-model="shell.name"
+                        @keyup.enter="addshell(shell)">
+                        <br>
+                    <input type="text" placeholder="shell"
+                        v-model="shell.shell"
+                        @keyup.enter="addshell(shell)">
+                </div>
+                <button @click="clean(shells)">clean</button>
+                <button @click="sendshells">send</button>
+            </div>
+
+            <div class="send software">
+                <div class="inputsoftware">
+                    <input type="text" placeholder="name"
+                        v-model="software.name" 
+                        @keyup.enter="addsoftware(software)">
+                </div>
+                <button @click="clean(softwares)">clean</button>
+                <button @click="sendsoftwares">send</button>
+            </div>
+        </div>
+
+        <div class="output">
+            <span class="show">
+                SHELL LIST
+                <p v-for="(s, i) in shells">
+                    ITEM:  {{ i }}
+                    <br>
+                    {{ s }}
+                </p>
+            </span>
+            <span class="show">
+                SOFTWARE LIST
+                <p v-for="(s, i) in softwares">
+                    ITEM: {{ i }} <br>
+                    {{ s }}
+                </p>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default{
+    data() {
+        return{
+            shells: [],
+            shell: {
+                name: null,
+                shell: null
+            },
+
+            softwares: [],
+            software:{
+                name: null,
+                start: false
+            }
+        }
+    },
+
+    methods: {
+        // add 
+        addshell(shell){
+            if (shell.name && shell.shell){
+                this.shells.push({...shell})
+                shell.name = null
+                shell.shell = null
+            }
+        },
+        addsoftware(software){
+            if (software.name){
+                this.softwares.push({...software})
+                software.name = null
+            }
+        },
+
+        // clean
+        clean(obj){
+            obj.length = 0
+        },
+
+        // send
+        sendshells(){
+            axios.put("/servers/send_control_shell", this.shells).then((res) =>{
+                this.shells = []
+            })
+        },
+        sendsoftwares(){
+            axios.put("/servers/send_software_checklist/", this.softwares).then((res)=>{
+                this.softwares = []
+            })
+        }
+    }
+}
+</script>
+
+
+<style>
+.sendbox {
+    float:left;
+    display: flex; 
+    flex-direction: column; 
+    align-items: center;
+}
+
+.sends{
+    display: flex;
+    align-items: start;
+    justify-items: space-around;
+    margin-bottom: 100px;
+}
+.send {
+    float: left;
+    margin: 10px 76px 10px 76px;
+
+}
+
+.output{
+    float: right;
+    display: flex;
+    align-items: flex-start;
+}
+
+.send input {
+    height: 18px;
+    width: 120px;
+    line-height: 18px;
+    border: #9ea4e6 ridge 3px;
+    border-radius: 5px;
+}
+
+.send.software input{
+    height: 42px;
+}
+
+
+/* ====== span style ====== */
+span.show {
+    display: inline-block;
+    width: 300px;
+    height: 320px;
+    overflow-y:auto;
+    /* line-height: 15px; */
+    background-color: #fabfab;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    text-align: center;
+    word-wrap: break-word;
+}
+
+/* span scrollbar */
+span.show::-webkit-scrollbar{
+    width: 10px;
+}
+span.show::-webkit-scrollbar-track{
+    background-color: #9ea4e6;
+    border-radius: 2px;
+}
+span.show::-webkit-scrollbar-thumb{
+    background-color: aqua;
+    border-radius: 5px;
+}
+
+/* span p */
+span.show p{
+    margin: 3px;
+}
+</style>
