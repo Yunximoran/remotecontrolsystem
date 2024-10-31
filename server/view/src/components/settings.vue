3c5bbec6a9a947fb4a8992119a9ee359ba19b224
yumo <2970368069@qq.com>
2024-10-31 23:16:14 +0800

    update view settings.vue

--- server/view/src/components/settings.vue
+++ server/view/src/components/settings.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="menu">
-        <span class="settings" @click="load_settings(document.querySelector('.settings'), options)">
+        <span ref="settings" class="settings" @click="loadoptions(this.$refs.settings, 'SETTINGS', options, 0)">
             <p>SETTINGS</p>
         </span>
         <span class="login">
@@ -11,10 +11,11 @@
 
 <script>
 import axios from "axios";
-
+import { createElementBlock } from "vue";
 export default{
     data(){
         return {
+            // 设置选项
             options:{
                 port:{
                     udp:{
@@ -30,12 +31,26 @@ export default{
                 },
                 demo: this.setdemo
             },
-            showsettings: false,
-            showoptions_level_1: false,
-            showoptions_level_2: false,
-            showoptions_level_3: false,
 
-            isHovered: false,
+            // 选项级别
+            option_level: [
+                'level_0',
+                "level_1",
+                "level_2",
+                "level_3",
+                "level_4",
+                "level_5",
+            ],
+
+            // 选项标记： 是否展示子选项
+            is_show_options: [
+                false,
+                false,
+                false,
+                false,
+                false,
+                false,
+            ]
         }
     },
     methods: {
@@ -44,31 +59,49 @@ export default{
                 this.setting = res.data
             })
         },
-        alter(){
-            this.isHovered = !this.isHovered
-            this.showoptions_level_3 = !this.showoptions_level_3
+
+        loadoptions(node, label, options, level){
+            this.is_show_options[level] = !this.is_show_options[level]
+            if (this.is_show_options[level]) {
+                this.addsettings(node, options, level)
+            }
+            else {
+                this.removesettings(node, label)
+            }
+
         },
 
-        load_settings(parent, options){
-            for (option in options){
-                current = document.createElement('span')
+        addsettings(parent, options, level){
+            for (let option in options){
+                let current = document.createElement('span')
+                let title = document.createElement('p')
 
-                title = document.createElement('p')
-                title.textConntext = option
-                
+                title.textContent = option
+
+                console.log(typeof level)
+                current.className = this.option_level[level]
                 current.appendChild(title)
 
-                if (typeof value == Object){
-                    this.load_settings(current, options[option])
+                if (typeof options[option] === 'object'){
+                    // 怎么设置同级别共用一个is_show
+                    current.onclick = (event) => {
+                        event.stopPropagation();
+                        this.loadoptions(current, option, options[option], level+1)
+                    }
                 }
                 else {
-                    current.onClick(options[option])
+                    current.onclick = () => this.alter(option)
                 }
 
                 parent.appendChild(current)
             }
         },
-
+        removesettings(element, label){
+            element.innerHTML = `<p>${label}</p>`
+        },
+        alter(option){
+            alert(`alter ${option} option`)
+        },
         setdemo(){
             console.log("demo settings")
             alert("demo func")
@@ -79,13 +112,22 @@ export default{
 
 <style>
 .menu{
+    user-select: none;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-items: center;
     height: 30px;
+    
+}
+.menu p, span{
+    background-color: #798589;
+    margin: 0;
+    /* margin: 0; */
 }
 
+
+/* settings  */
 .menu span{
     width: 120px;
     height: 100%;
@@ -93,7 +135,26 @@ export default{
     color: #c02e2e;
     text-align: center;
     line-height: 30px;
-    padding: 3px;
     border-radius: 3px;
+
+}
+span.level_0{
+    position: relative;
+    margin: 0;
+    color: gold
+}
+span.level_1{
+    position: relative;
+    bottom: 30px;
+    left: 120px;
+    margin: 0;
+    color: rebeccapurple
+}
+span.level_2{
+    position: relative;
+    bottom: 30px;
+    left: 120px;
+    margin: 0;
+    color: green
 }
 </style>
