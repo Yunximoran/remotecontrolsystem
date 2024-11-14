c70366aef66beb4e18871517ccf38ce8faf517d7
yumo <2970368069@qq.com>
2024-11-14 21:21:55 +0800

    alter app.vue path

---
+++ server/view/src/App.vue
@@ -0,0 +1,92 @@
+
+<template>
+   <!-- menu  -->
+    <div class="menu">
+        <Settings></Settings>
+        <Login></Login>
+    </div>
+    <!-- 基础控制按钮 -->
+    <!-- content -->
+    <div class="core">
+        <Sendmodel style="
+        margin-right:80px;
+        "
+        ></Sendmodel>
+        <ClientBox></ClientBox>
+    </div>
+</template>
+
+
+<script>
+import Sendmodel from "./components/sendmodel.vue";
+import Settings from "./components/menu/settings.vue";
+import Login from "./components/menu/login.vue";
+import ClientBox from "./components/home/clientview/clientbox.vue"
+
+
+export default{
+    components: {
+        Sendmodel,
+        Settings,
+        ClientBox,
+        Login,
+    },
+    data(){
+        return {
+            clients: {}
+        }
+    },
+    created(){
+        
+    }
+}
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+
+/* global */
+body {
+    background-color: #a29bc2;
+    /* position: relative; */
+}
+
+button {
+  background-color: rgb(175, 220, 171);
+  color:rgb(21, 73, 55);
+  border-radius: 3px;
+}
+
+.menu{
+    /* position:relative; */
+    user-select: none;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-items: center;
+    height: 30px;
+    
+}
+.menu p, span{
+    background-color: #798589;
+    margin: 0;
+}
+
+.core {
+    display: flex;
+    float: right;
+    margin-left: 10px;
+    justify-items: flex-end;
+}
+
+
+</style>
