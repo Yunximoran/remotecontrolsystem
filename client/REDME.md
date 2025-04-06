2ff1fd025ef4d6d2a506b0bad8849f6c31bed2aa
yumo <2970368069@qq.com>
2025-04-06 21:00:00 +0800

    update

--- client/REDME.md
+++ client/REDME.md
@@ -36,7 +36,39 @@ git clone https://github.com//Yunximoran/remotecontrolsystem.git
 ```
 ##### 终端运行
 ```bash
-  * python init.py --ip_server=[服务端IP地址]， --broadcast=[广播域]， --net=[指定网卡名称]
-    * python init.py --ip_server=192.168.31.176 --broadcast=192.168.31.255 --net=WLAN
+  * python init.py --ip_server=[服务端IP地址]， --broadcast=[广播域]， --net=[指定网卡名称] --username=[计算机用户] --password=[用户密码]
+    * python init.py --ip_server=192.168.31.176 --broadcast=192.168.31.255 --net=WLAN --username=root, --password=123456
   * python main.py
+```
+
+
+##### 开机自启动
+##### Linux：
+```bash
+sudo systemctl daemon-reload
+sudo vim /etc/systemd/system/[remotecontrol].service
+  `vim remotecontrol.service
+  [Unit]
+  Description=remotecontrol
+  After=network.target
+
+  [Service]
+  ExecStart=[指定python解释器路径] [path/to/client/main.py]
+  WrokingDirectory=[path/to/client]
+  Restart=always
+  User=[root]
+
+  [Install]
+  WantedBy=multi-user.target
+  # 替换[]中的内容
+  `
+sudo systemctl enable [remotecontrol]
+sudo systemctl start [remotecontrol]
+```
+
+##### Windows 
+```bass
+打开注册表：HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run
+右键右侧空白区域 → 新建 → 字符串值："[指定python解释器]" "[/path/to/client/main.py]" * 引号不能省略
+
 ```
--- client/init.py
+++ client/init.py
@@ -17,11 +17,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--ip_server', help='设置服务端IPIP')
 parser.add_argument('--broadcast', help='设置广播域')
 parser.add_argument('--net', help="设置客户端网卡")
+parser.add_argument("--username", help="计算机用户名称")
+parser.add_argument("--password", help="计算机用户密码")
 args = parser.parse_args()
 
 IP_SERVER = args.ip_server    # 服务端地址
 BROADCAST = args.broadcast    # 广播域名
 NET = NetWork(args.net)       # 指定网卡
+USERNAME = args.username
+PASSWORD = args.password
 
 
 if __name__ == "__main__":
@@ -32,17 +36,14 @@ if __name__ == "__main__":
         sock = resolver("sock")
 
         # 初始本机信息
+        computer.setattrib("username", USERNAME)
+        computer.setattrib("password", PASSWORD)
         computer.search("name").settext(platform.node())
         computer.search("os").settext(platform.system())
         computer.search("version").settext(platform.version())
         computer.search("machine").settext(platform.machine())
         computer.search("ram").settext(f"{psutil.virtual_memory().total / (1024 ** 3):.2f}")
 
-        # 初始化CPU信息
-        cpu.search("processor").settext(platform.processor())
-        cpu.search("physical-cores").settext(psutil.cpu_count(logical=False))
-        cpu.search("logical-cores").settext(psutil.cpu_count(logical=True))
-        cpu.search("architecture").settext(platform.architecture()[0])
 
         # 设置服务端IP
         net.search("ip-server").settext(IP_SERVER)
